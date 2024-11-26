3da2a5911e2e1ab3b231bc63c171025ec0d27876
vinhdv2k4 <laivinh2906@gmail.com>
2024-11-26 16:18:53 +0700

    bai 4

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -4,6 +4,8 @@
         private String director;
         private int length;
         private float cost;
+        private static int nbDigitalVideoDiscs = 0;
+        private int id;
         
         public String getTitle() {
         return title;
@@ -23,20 +25,26 @@
         public void setTitle(String title) {
             this.title = title;
         }
+        public int getID(){
+            return id;
+        }
     
         public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDiscs;
         }
         public DigitalVideoDisc(String title) {
         this.title = title;
+        this.id = ++nbDigitalVideoDiscs;
         }
         public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDiscs;
         }
         public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -44,14 +52,17 @@
         this.director = director;
         this.length = length;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDiscs;
         }
         public DigitalVideoDisc(String title, String category, int length, float cost) {
             this.title = title;
             this.category = category;
             this.length = length;
             this.cost = cost;
+            this.id = ++nbDigitalVideoDiscs;
         }
-    
         
-    }
+    
+}
+
     
